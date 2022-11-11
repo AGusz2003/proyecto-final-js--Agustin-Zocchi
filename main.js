cf37da6dc9942f010b99889884fa02362a31735c
Agustin Zocchi <agusz2003@gmail.com>
2022-11-11 15:59:36 -0300

    proyecto final js

--- main.js
+++ main.js
@@ -1,114 +1,118 @@
-// arrays con cada producto para seleccioanr
-const products = [
-    {
-      id: 1,
-      nombre:'FIFA 23',
-      precio: 69.99,
-      img:'./img/fifa-23-cover-star-kylian-mbappe.jpg',
-    },
-    {
-      id: 2,
-      nombre:'NBA 2k23',
-      precio: 69.99,
-      img:'./img/nba-2k23.jpg',
-    },
-    {
-      id: 3,
-      nombre:'SPIDERMAN MILES MORALE',
-      precio: 49.99,
-      img:'./img/spiderman.jpg',
-    },
-    {
-      id: 4,
-      nombre:'God of War Ragnarök',
-      precio: 69.99,
-      img:'./img/GodofWarRagnarök.webp',
-    },
-    {
-      id: 5,
-      nombre:'Resident Evil 4',
-      precio: 59.99,
-      img:'./img/resident-evil4.png',
-    },
-    {
-      id: 6,
-      nombre:'NFL PRO ERA',
-      precio: 29.99,
-      img:'./img/nfl-pro-era.png',
-    },
-    {
-        id: 7,
-        nombre:'Hogwarts Legacy',
-        precio: 69.99,
-        img:'./img/Hogwarts-Legacy.webp',
-      },
-      {
-        id: 8,
-        nombre: 'Grand Theft Auto V',
-        precio: 19.79,
-        img:'./img/gtav.webp',
-      },
-      {
-        id: 9,
-        nombre: 'Call of Duty®: Modern Warfare®',
-        precio: 59.99,
-        img:'./img/Call-of-Duty-Modern-Warfare.webp',
-      },
-      {
-        id: 10,
-        nombre: 'The Last of Us Part II',
-        precio: 59.59,
-        img:'./img/TheLastofUsPart-II.webp',
-      },
-      {
-        id: 11,
-        nombre: 'PGA TOUR 2K23',
-        precio: 59.59,
-        img:'./img/PGA-TOUR-2K23.webp',
-      },
-      {
-        id: 12,
-        nombre: 'NHL 23',
-        precio: 59.59,
-        img:'./img/NHL23.jfif',
-      },
-      {
-        id: 13,
-        nombre: 'MLB® The Show™',
-        precio: 39.99,
-        img:'./img/MLB.jfif',
-      },
-      {
-        id: 14,
-        nombre: 'Battlefield™ 2042',
-        precio: 53.59,
-        img:'./img/Battlefield-2042.jfif',
-      },
-      {
-        id: 15,
-        nombre: 'DARK SOULS',
-        precio: 59.99,
-        img:'./img/dark-soul.jfif',
-      },
-  ];
-  
-  products.forEach((product) => {
-    document.getElementById('lista-productos').innerHTML += `
-  
-    <div class="card" style="width: 15rem;">
-  
-        <img src=${product.img} class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">${product.nombre}</h5>
-          <p class="precio">  <span class="u-pull-right ">US$${product.precio}</span></p>
-          <a href="#" class="btn btn-primary agregar-carrito" data-id="${product.id}">Agregar al Carrito</a>
-        </div>
-    </div>
+// // arrays con cada producto para seleccioanr
+// const products = [
+//     {
+//       id: 1,
+//       nombre:'FIFA 23',
+//       precio: 69.99,
+//       img:'./img/fifa-23-cover-star-kylian-mbappe.jpg',
+//     },
+//     {
+//       id: 2,
+//       nombre:'NBA 2k23',
+//       precio: 69.99,
+//       img:'./img/nba-2k23.jpg',
+//     },
+//     {
+//       id: 3,
+//       nombre:'SPIDERMAN MILES MORALE',
+//       precio: 49.99,
+//       img:'./img/spiderman.jpg',
+//     },
+//     {
+//       id: 4,
+//       nombre:'God of War Ragnarök',
+//       precio: 69.99,
+//       img:'./img/GodofWarRagnarök.webp',
+//     },
+//     {
+//       id: 5,
+//       nombre:'Resident Evil 4',
+//       precio: 59.99,
+//       img:'./img/resident-evil4.png',
+//     },
+//     {
+//       id: 6,
+//       nombre:'NFL PRO ERA',
+//       precio: 29.99,
+//       img:'./img/nfl-pro-era.png',
+//     },
+//     {
+//         id: 7,
+//         nombre:'Hogwarts Legacy',
+//         precio: 69.99,
+//         img:'./img/Hogwarts-Legacy.webp',
+//       },
+//       {
+//         id: 8,
+//         nombre: 'Grand Theft Auto V',
+//         precio: 19.79,
+//         img:'./img/gtav.webp',
+//       },
+//       {
+//         id: 9,
+//         nombre: 'Call of Duty®: Modern Warfare®',
+//         precio: 59.99,
+//         img:'./img/Call-of-Duty-Modern-Warfare.webp',
+//       },
+//       {
+//         id: 10,
+//         nombre: 'The Last of Us Part II',
+//         precio: 59.59,
+//         img:'./img/TheLastofUsPart-II.webp',
+//       },
+//       {
+//         id: 11,
+//         nombre: 'PGA TOUR 2K23',
+//         precio: 59.59,
+//         img:'./img/PGA-TOUR-2K23.webp',
+//       },
+//       {
+//         id: 12,
+//         nombre: 'NHL 23',
+//         precio: 59.59,
+//         img:'./img/NHL23.jfif',
+//       },
+//       {
+//         id: 13,
+//         nombre: 'MLB® The Show™',
+//         precio: 39.99,
+//         img:'./img/MLB.jfif',
+//       },
+//       {
+//         id: 14,
+//         nombre: 'Battlefield™ 2042',
+//         precio: 53.59,
+//         img:'./img/Battlefield-2042.jfif',
+//       },
+//       {
+//         id: 15,
+//         nombre: 'DARK SOULS',
+//         precio: 59.99,
+//         img:'./img/dark-soul.jfif',
+//       },
+//   ];
+  fetch("./productos.json")
+  .then(respuestas=>respuestas.json())
+  .then(products=>{
+    products.forEach((product) => {
+      document.getElementById('lista-productos').innerHTML += `
     
-    `;
-  });
+      <div class="card" style="width: 15rem;">
+    
+          <img src=${product.img} class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${product.nombre}</h5>
+            <p class="precio">  <span class="u-pull-right ">US$${product.precio}</span></p>
+            <a href="#" class="btn btn-primary agregar-carrito" data-id="${product.id}">Agregar al Carrito</a>
+          </div>
+      </div>
+      
+      `;
+    });
+  
+  })
 
-
+  
 
 // listado de las variables creadas
 
@@ -149,13 +153,26 @@ function agregarProductos(e) {
     if(e.target.classList.contains("agregar-carrito")) {
         const productoSeleccionado = e.target.parentElement.parentElement;
         datosProducto(productoSeleccionado)
-        
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'se ha añadido el juego correctamente al carrito de compras',
+          showConfirmButton: false,
+          timer: 1500
+        })
     }
 }
 // Esta funcion elimina un juego seleccionado
 function eliminarProducto(e) {
     if(e.target.classList.contains("borrar-producto")) {
         const productoId =e.target.getAttribute("data-id");
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'se ha eliminado el juego correctamente de tu carrito de compras',
+          showConfirmButton: false,
+          timer: 1500
+        })
         
 
         //Elimina del carrito por id
